0ba1887ab59d03c67246ee90dee7c5e0f6e962b9
Evyatar_BenAmi <evyatar2091@gmail.com>
2026-02-11 09:56:13 +0200

    cheng name file Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './components/header'
+import Header from './components/Header'
 import ProfileCard from './components/ProfileCard'
 import Footer from './components/Footer'
 
